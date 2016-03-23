7214a0ee467d2a729c3394d103460513b8d8f1a7
Vadim&Chris <Vadim&Chris@aa.io>
2016-03-23 11:06:07 -0400

    Inital commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  root to: "static_pages#root"
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+
+end
